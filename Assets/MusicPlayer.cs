5501055076e1ddeca4c371a84d1a010c2ae7b5e2
sahil.naran <sahil.naran6@gmail.com>
2020-09-07 12:22:34 +0200

    inktober 2019 initial

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public AudioClip[] audioClips;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return audioClips[Random.Range(0, audioClips.Length)];
+    }
+}
